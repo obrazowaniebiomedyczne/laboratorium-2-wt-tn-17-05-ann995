95fe72d010c8292c504e40c61f64ea776f7359d9
ann995 <37317627+ann995@users.noreply.github.com>
2018-11-20 18:32:00 +0100

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,23 +3,41 @@ import numpy as np
 
 # Zadanie na ocenę dostateczną
 def renew_pictures():
+    img = []
+	kernel = np.ones((3, 3), np.uint8)
+	kernel = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]], np.uint8)
 
-    # ---------------
-    # Do uzupełnienia
-    # ---------------
+    	for i in range(1, 5):
+		if (i == 1):
+		    img.append(cv.imread('figures/crushed.png', 0))
+		else:
+		    img.append(cv.imread('figures/crushed'+str(i)+'.png', 0))
+		
+		opening = cv.morphologyEx(img[i-1], cv.MORPH_OPEN, kernel, iterations=1)
+		closing = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel, iterations=1)
+		opening = cv.morphologyEx(closing, cv.MORPH_OPEN, kernel, iterations=2)
+		closing = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel, iterations=2)
+		opening = cv.morphologyEx(opening, cv.MORPH_OPEN, kernel, iterations=3)
+		closing = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel, iterations=2)
+		
+		if (i == 1):
+		    cv.imwrite('figures/result_crushed.jpg', closing)
+		else:
+		    cv.imwrite('figures/result_crushed'+str(i)+'.png', closing)
 
     pass
 
 
 # Zadanie na ocenę dobrą
 def own_simple_erosion(image):
-    new_image = np.zeros(image.shape, dtype=image.dtype)
+    kernel = np.array([[0, 1, 0],
+                    [1, 1, 1],
+                    [0, 1, 0]], np.uint8)
+    erosion = cv2.erode(image,kernel,iterations = 5)
 
-    # ---------------
-    # Do uzupełnienia
-    # ---------------
+return erosion
 
-    return new_image
+
 
 
 # Zadanie na ocenę bardzo dobrą
